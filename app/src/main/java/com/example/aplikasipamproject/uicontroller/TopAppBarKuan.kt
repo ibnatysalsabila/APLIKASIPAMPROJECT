1164496f22fab8c85d632779802728c1b4c78a52
ibnatysalsabila <salsajpr01@gmail.com>
2026-01-15 18:16:56 +0700

    update topappbarkuan

--- app/src/main/java/com/example/aplikasipamproject/uicontroller/TopAppBarKuan.kt
+++ app/src/main/java/com/example/aplikasipamproject/uicontroller/TopAppBarKuan.kt
@@ -11,15 +11,6 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
-/**
- * Composable yang menampilkan top app bar dan dapat digulir.
- *
- * @param title Judul yang akan ditampilkan di tengah TopAppBar.
- * @param canNavigateBack Boolean untuk menentukan apakah tombol kembali ditampilkan.
- * @param modifier Modifier untuk kustomisasi.
- * @param scrollBehavior Perilaku gulir untuk TopAppBar.
- * @param navigateUp Aksi yang dijalankan saat tombol kembali ditekan.
- */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopAppBarKuan(
