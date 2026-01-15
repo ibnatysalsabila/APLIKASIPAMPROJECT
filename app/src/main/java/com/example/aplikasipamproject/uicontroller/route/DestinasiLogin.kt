253c0e3ae1dc1cc876b08e2d30272565ed208983
ibnatysalsabila <salsajpr01@gmail.com>
2026-01-15 12:46:28 +0700

    menambahkan destinasi navigasi untuk halaman Login dan Home

---
+++ app/src/main/java/com/example/aplikasipamproject/uicontroller/route/DestinasiLogin.kt
@@ -0,0 +1,14 @@
+package com.example.aplikasipamproject.uicontroller.route
+
+/**
+ * Mendefinisikan route untuk halaman Login.
+ */
+object DestinasiLogin : DestinasiNavigasi {
+    override val route = "login"
+    override val titleRes = "Login"
+}
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Halaman Utama"
+}
